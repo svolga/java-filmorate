f438893a08ef0364f2fd653431c744cb46f38f45
Ракчеев Сергей <rakcheev.sg@magnat.ru>
2023-07-19 00:52:42 +0300

    fix: Check style

--- src/main/java/ru/yandex/practicum/filmorate/service/Manager.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/Manager.java
@@ -1,14 +1,10 @@
 package ru.yandex.practicum.filmorate.service;
 
 import ru.yandex.practicum.filmorate.exception.ValidateException;
-
 import java.util.List;
 
-public interface Manager <T> {
-
+public interface Manager<T> {
     T create(T t);
-
     T update(T t) throws ValidateException;
-
     List<T> getAll();
 }
