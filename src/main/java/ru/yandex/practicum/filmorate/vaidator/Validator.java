f94edaee8c249b2f954d739622cd2fd9df3f6bba
Ракчеев Сергей <rakcheev.sg@magnat.ru>
2023-07-19 00:39:24 +0300

    fix: Убрать неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/vaidator/Validator.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.vaidator;
-
-import ru.yandex.practicum.filmorate.exception.ValidateException;
-
-public interface Validator<T> {
-    void validate(T value) throws ValidateException;
-}
