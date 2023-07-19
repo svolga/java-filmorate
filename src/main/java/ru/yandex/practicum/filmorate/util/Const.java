7fdc89b354767915f656f66e773ea966ba877958
Ракчеев Сергей <rakcheev.sg@magnat.ru>
2023-07-19 08:50:23 +0300

    fix: Константы

--- src/main/java/ru/yandex/practicum/filmorate/util/Const.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/Const.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.util;
 
 public class Const {
-    public final static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    public final static String DATE_FORMAT = "yyyy-MM-dd";
-    public final static String MIN_FILM_DATE = "1895-12-28";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String MIN_FILM_DATE = "1895-12-28";
 }
