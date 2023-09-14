ecde19f85429ad8c3439c65e2450db618f5fddd2
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-09-14 12:33:46 +0300

    style: Исправить style

--- src/main/java/ru/yandex/practicum/filmorate/exception/FeedNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FeedNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class FeedNotFoundException extends RuntimeException{
+public class FeedNotFoundException extends RuntimeException {
     public FeedNotFoundException(String message) {
         super(message);
     }
