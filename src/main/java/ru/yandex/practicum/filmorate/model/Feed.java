b7d3235e60fdcb6f61d999e8ac93e29981136b3f
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-09-14 12:37:37 +0300

    style: Исправить style

--- src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -7,9 +7,7 @@ import ru.yandex.practicum.filmorate.util.EventType;
 import ru.yandex.practicum.filmorate.util.Operation;
 
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Data
 @Builder
