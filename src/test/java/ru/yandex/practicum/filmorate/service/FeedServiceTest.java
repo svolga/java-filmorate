bfe99f0b191be310add2fefadf6a8dc1fa65aa44
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-09-15 17:21:00 +0300

    style: Исправить неисп. импорт

--- src/test/java/ru/yandex/practicum/filmorate/service/FeedServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FeedServiceTest.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
